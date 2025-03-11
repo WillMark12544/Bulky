b1a4236dab7090da354c0ded410efffa6de54c61
WillMark12544 <williamjosephmark@gmail.com>
2025-03-12 11:36:29 +1300

    Week 1 Done

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using BulkyWeb.Migrations;
 
 namespace BulkyWeb.Controllers
 {
+    
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -12,11 +13,21 @@ namespace BulkyWeb.Controllers
         {
             _db = db;
         }
-
         public IActionResult Index()
         {
             List<Category> objCategoryList= _db.Categories.ToList();
             return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
